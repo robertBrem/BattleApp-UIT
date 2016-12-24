ec046901a0e5305d842325836428eacb7ce65d45
Robert Brem <brem_robert@hotmail.com>
2016-12-24 10:21:44 +0100

    Initial commit

---
+++ src/test/java/ninja/disruptor/battleapp/BattleAppIT.java
@@ -0,0 +1,28 @@
+package ninja.disruptor.battleapp;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.page.InitialPage;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+@RunAsClient
+@RunWith(Arquillian.class)
+public class BattleAppIT {
+
+    @Drone
+    WebDriver browser;
+
+    @Test
+    public void shouldContainRobert(@InitialPage BattleAppPage page) {
+        String expectedToContain = "robert";
+        String content = browser.getPageSource();
+        assertThat(content, containsString(expectedToContain));
+    }
+
+}
---
+++ src/test/java/ninja/disruptor/battleapp/BattleAppPage.java
@@ -0,0 +1,7 @@
+package ninja.disruptor.battleapp;
+
+import org.jboss.arquillian.graphene.page.Location;
+
+@Location("http://disruptor.ninja:31080/battleapp/resources/users")
+public class BattleAppPage {
+}
